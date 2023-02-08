5904c5f3d28c0bfe9f2e0e5e119d8bcc0a77c959
TamzinC <115940593+TamzinC@users.noreply.github.com>
2023-02-08 22:12:52 +0000

    completed contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -9,15 +9,21 @@ const styles = {
     backgroundColor: 'lightseagreen',
     color: '#fff',
     cursor: 'pointer'
+  },
+  success: {
+    textAlign: 'center',
+    color: 'lightseagreen',
+    marginTop: '15px'
   }
 };
 
 function Contact(props) {
-
+  const [success, setSuccess] = useState(false);
   const [formData, setFormData] = useState({
     name: '',
     email: '',
-    message: ''
+    message: '',
+    access_key: '0c0b524a-4b70-4633-8d60-aaa6937c29ef'
   }) 
 
   const handleChange = (event) => {
@@ -29,21 +35,67 @@ function Contact(props) {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log('form submitted!')
-    console.log(formData)
+
+    const data = JSON.stringify(formData);
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: data
+        }).then(res => res.json())
+          .then(data => {
+            setSuccess(true);
+            setFormData({
+              ...formData,
+              name: '',
+              email: '',
+              message: ''
+            })
+
+            setTimeout(() => {
+              setSuccess(false);
+            }, 3000);
+          })
+          .catch(err => console.log(err));
+
+
+
+      console.log('form submitted!')
+      console.log(formData)
   };
   
     return (
       <>
         <h1 style={styles.header}>Contact Me</h1>
 
+
         <form onSubmit={handleSubmit}>
-          <input name='name' onChange={handleChange} type='text' placeholder='Enter your name' />
-          <input name='email' onChange={handleChange} type='text' placeholder='Enter your Email Address' />
-          <textarea name='message' onChange={handleChange} placeholder='Enter your message' cols='30' rows='10'></textarea>
+          <input 
+            name='name' 
+            value={formData.name} 
+            onChange={handleChange} 
+            type='text' 
+            placeholder='Enter your name' />
+          <input 
+            name='email' 
+            value={formData.email} 
+            onChange={handleChange} 
+            type='text' 
+            placeholder='Enter your Email Address' />
+          <textarea 
+            name='message' 
+            value={formData.message} 
+            onChange={handleChange} 
+            placeholder='Enter your message' 
+            cols='30' 
+            rows='10'></textarea>
           <button style={(styles.btn)}>Submit</button>
         </form>
 
+        {success && <p style={styles.success}>Form Submitted Sucessfully!</p>}
       </>
     )
   }
