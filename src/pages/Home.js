4c8e07dce495997ba5b81a8efd102b36c5ab5ac8
TamzinC <115940593+TamzinC@users.noreply.github.com>
2023-02-10 00:22:35 +0000

    further changes made to styling of home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,19 @@ import profilePic from '../images/profile-pic.PNG';
 function Home(props) {
     const styles = {
         hero: {
-            display: 'flex',
-            padding: '20px',
+            padding: '50px',
             backgroundColor: '#EBD7C1'
         },
+        img: {
+            display: 'block',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            borderRadius: '50%',
+            height: '300px',
+            width: '320px'
+        },
         intro: {
-            alignSelf: 'center',
+            marginTop: '30px',
             textAlign: 'center',
             lineHeight: '40px',
             color: '#462920'
@@ -20,24 +27,19 @@ function Home(props) {
             border: 'none',
             borderRadius: '5px',
             width: '100px'
-        },
-        img: {
-            borderRadius: '50%',
-            height: '300px',
-            width: '320px'
         }
       }
     
     return (
         <div className='hero' style={styles.hero}>
+            <div>
+                <img src={profilePic} alt="profile" style={styles.img} />
+            </div>
             <div className='intro' style={styles.intro}>
                 <h1>Hey, I'm Tamzin!</h1>
                 <p>Front-End Web Developer</p>
                 
                 <button style={styles.button} href="mailto:tamzin.chikhalia@gmail.com" role="button">Contact Me</button>
-            </div>
-            <div>
-                <img src={profilePic} alt="profile" style={styles.img} />
             </div>
         </div>
     );
