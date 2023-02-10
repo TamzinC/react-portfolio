fa1dd796fe2192616b8d325c86dabc6a2e924c82
TamzinC <115940593+TamzinC@users.noreply.github.com>
2023-02-10 00:02:56 +0000

    adding styling to home page and nav

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,43 @@
 import profilePic from '../images/profile-pic.PNG';
 
 function Home(props) {
-  
+    const styles = {
+        hero: {
+            display: 'flex',
+            padding: '20px',
+            backgroundColor: '#EBD7C1'
+        },
+        intro: {
+            alignSelf: 'center',
+            textAlign: 'center',
+            lineHeight: '40px',
+            color: '#462920'
+        },
+        button: {
+            backgroundColor: 'rgb(128, 108, 84)',
+            color: '#fff',
+            height: '40px',
+            border: 'none',
+            borderRadius: '5px',
+            width: '100px'
+        },
+        img: {
+            borderRadius: '50%',
+            height: '300px',
+            width: '320px'
+        }
+      }
+    
     return (
-        <div>
-            <div>
+        <div className='hero' style={styles.hero}>
+            <div className='intro' style={styles.intro}>
                 <h1>Hey, I'm Tamzin!</h1>
                 <p>Front-End Web Developer</p>
-                <hr/>
-                <button href="mailto:tamzin.chikhalia@gmail.com" role="button">Contact Me</button>
+                
+                <button style={styles.button} href="mailto:tamzin.chikhalia@gmail.com" role="button">Contact Me</button>
             </div>
             <div>
-                <img src={profilePic} alt="profile"/>
+                <img src={profilePic} alt="profile" style={styles.img} />
             </div>
         </div>
     );
