084f547ad2247f7d459ab2ff0a25d19b2b52d4b9
TamzinC <115940593+TamzinC@users.noreply.github.com>
2023-02-11 00:07:45 +0000

    adding some basic info and layout to the about me page:

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,10 +1,45 @@
 
 
 function Gallery(props) {
+  const styles = {
+    about: {
+      display: 'flex',
+      justifyContent: 'space-between'
+    },
+    contact: {
+      display: 'flex',
+      flexDirection: 'column',
+    }
+  }
   
     return (
       <main>
         <h1>About Me</h1>
+
+        <div style={styles.about}>
+          <p>Coming from a Marketing background, I have experience in a variety of different aspects ranging from graphic design, marketing strategy and paid social advertising, as well as E-Commerce which involves daily management of a live website. I'm now looking to broaden my career by moving into web development and finding my next challenge!</p>
+
+          <aside style={styles.contact}>
+            <a>GitHub</a>
+            <a>LinkedIn</a>
+            <a href="mailto:tamzin.chikhalia@gmail.com">Email</a>
+            <a>CV</a>
+          </aside>
+
+          <section>
+            <ul>
+              <h3>Skills</h3>
+              <li>HTML5</li>
+              <li>CSS</li>
+              <li>Javascript</li>
+              <li>JQuery</li>
+              <li>Bootstrap</li>
+              <li>moment.js</li>
+              <li>React</li>
+              <li>Node.js</li>
+            </ul>
+          </section>
+        </div>
 
       </main>
     )
