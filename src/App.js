8edfc1a59d5d3a753aacac961733ab97975cb572
TamzinC <115940593+TamzinC@users.noreply.github.com>
2025-07-17 23:18:43 +0100

    updated footer layout and styling

--- src/App.js
+++ src/App.js
@@ -1,40 +1,53 @@
 // import {useState} from 'react';
-import {Routes, Route} from 'react-router-dom';
-import data from './data.json';
-import './index.css';
+import { Routes, Route } from "react-router-dom";
+import data from "./data.json";
+import "./index.css";
 
-import Header from './components/Header/Header';
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Projects from './pages/Projects/Projects';
-import Contact from './pages/Contact/Contact';
-import NotFound from './pages/NotFound/NotFound.module.css/NotFound';
-import Footer from './components/Footer/Footer';
-
-
-
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Projects from "./pages/Projects/Projects";
+import Contact from "./pages/Contact/Contact";
+import NotFound from "./pages/NotFound/NotFound.module.css/NotFound";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  const title = "Tamzin Chikhalia"
+  const title = "Tamzin Chikhalia";
 
   return (
     <>
+      <svg width="0" height="0">
+        <defs>
+          <linearGradient
+            id="rainbow-gradient"
+            x1="0%"
+            y1="0%"
+            x2="100%"
+            y2="0%"
+          >
+            <stop offset="0%" stopColor="red" />
+            <stop offset="14%" stopColor="orange" />
+            <stop offset="28%" stopColor="yellow" />
+            <stop offset="42%" stopColor="green" />
+            <stop offset="57%" stopColor="blue" />
+            <stop offset="71%" stopColor="indigo" />
+            <stop offset="85%" stopColor="violet" />
+            <stop offset="100%" stopColor="red" />
+          </linearGradient>
+        </defs>
+      </svg>
 
       <Header title={title} />
 
-
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/projects' element={<Projects challenges={data}/>} />
-        <Route path='*' element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects challenges={data} />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-      
 
       <Footer title={title} />
-
-
     </>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,40 +1,114 @@
-import styles from './Footer.module.css';
+import {useEffect, useState, useRef} from "react";
+import {useLocation} from "react-router-dom";
+import styles from "./Footer.module.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope, faFilePdf } from "@fortawesome/free-solid-svg-icons";
 
+
+
 function Footer(props) {
-    return (
-      <footer>
-        <p>{props.title} &copy;{new Date().getFullYear()}</p>
-        <div className={styles.socialIcons}>
-          <a href="https://github.com/TamzinC" target="_blank">
-          <FontAwesomeIcon
-          icon={faGithub}
-          className={styles.socialBadge}></FontAwesomeIcon>
-          </a>
+  // console.log("styles", styles);
+  const [visible, setVisible] = useState(true);
+  const scrollTimeoutRef = useRef(null);
+  const prevScrollYRef = useRef(0);
+  const location = useLocation();
 
-          <a href="https://www.linkedin.com/in/tamzin-chikhalia-44a59a188/" target="_blank">
-          <FontAwesomeIcon
-          icon={faLinkedin}
-          className={styles.socialBadge}></FontAwesomeIcon>
-          </a>
+  const resetTimer = () => {
+    clearTimeout(scrollTimeoutRef.current);
+    scrollTimeoutRef.current = setTimeout(() => {
+      setVisible(false);
+    }, 3000);
+  };
 
-          <a href="mailto:tamzinchikhalia@gmail.com" target="_blank">
-          <FontAwesomeIcon
-          icon={faEnvelope}
-          className={styles.socialBadge}></FontAwesomeIcon>
-          </a>
+  const showFooter = () => {
+    setVisible(true);
+    resetTimer();
+  };
 
-          <a href="./assets/tamzin_cv.pdf" target="_blank">
-          <FontAwesomeIcon
-          icon={faFilePdf}
-          className={styles.socialBadge}></FontAwesomeIcon>
-          </a>
-        </div>
-      </footer>
-    )
-  }
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      const isScrollingUp = currentScrollY < prevScrollYRef.current;
+
+      if (isScrollingUp) {
+        showFooter();
+      }
+
+      prevScrollYRef.current = currentScrollY;
+    };
+
+    const handleMouseMove = () => {
+      showFooter();
+    };
+
+    const handleTouch = () => {
+      showFooter();
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("touchstart", handleTouch);
+    window.addEventListener("touchmove", handleTouch);
+    
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("touchstart", handleTouch);
+      window.removeEventListener("touchmove", handleTouch);
+      clearTimeout(scrollTimeoutRef.current);
+    };
+  }, []);
   
-  export default Footer;
+  useEffect(() => {
+    showFooter();
+  }, [location]);
+
+
+  return (
+    <footer className={`${styles.footer} ${visible ? styles.footerVisible : ""}`}>
+      <div style={{width: "100px"}}></div> {/* Placeholder for spacing */}
+
+      <div className={styles.footerTxt}>
+        <p className={styles.caption}>Made with ❤️ by {props.title}</p>
+        <p className={styles.copyright}>&copy;{new Date().getFullYear()}</p>
+      </div>
+
+      <div className={styles.socialIcons}>
+        <a href="https://github.com/TamzinC" target="_blank" className={styles.iconLink}>
+          <FontAwesomeIcon
+            icon={faGithub}
+            className={styles.socialBadge}
+          ></FontAwesomeIcon>
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/tamzin-chikhalia-44a59a188/"
+          target="_blank" className={styles.iconLink}
+        >
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            className={styles.socialBadge}
+          ></FontAwesomeIcon>
+        </a>
+
+        <a href="mailto:tamzinchikhalia@gmail.com" target="_blank" className={styles.iconLink}>
+          <FontAwesomeIcon
+            icon={faEnvelope}
+            className={styles.socialBadge}
+          ></FontAwesomeIcon>
+        </a>
+
+        <a href="./assets/tamzin_cv.pdf" target="_blank" className={styles.iconLink}>
+          <FontAwesomeIcon
+            icon={faFilePdf}
+            className={styles.socialBadge}
+          ></FontAwesomeIcon>
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
