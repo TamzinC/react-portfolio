8565cca867eaee9a2a96f8451ded3da4b10521c5
TamzinC <115940593+TamzinC@users.noreply.github.com>
2025-07-21 22:11:30 +0100

    updated icon + added link to 'home' in header. updated profile pic and caption/taglines in homepage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,41 @@
-import {NavLink} from 'react-router-dom';
-import styles from './Header.module.css';
+import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css";
 
 function Header(props) {
-    const getNavLinkClass = ({isActive}) =>
-        isActive ? styles.navLinkActive : styles.navLink;
-   
-    return (
-        <header className={styles.header}>
-            <div className={styles.logo}>
-                <img src='./images/profile-pic.PNG' alt="profile" className={styles.headerIcon} />
-                <h3 className={styles.headerName}>{props.title}</h3>
-            </div>
+  const getNavLinkClass = ({ isActive }) =>
+    isActive ? styles.navLinkActive : styles.navLink;
 
-            <nav>
-                <NavLink to='/'className={getNavLinkClass}>Home</NavLink>
-                <NavLink to='/about' className={getNavLinkClass}>About Me</NavLink>
-                <NavLink to='/projects' className={getNavLinkClass}>Projects</NavLink>
-                <NavLink to='/contact' className={getNavLinkClass}>Contact</NavLink>
-            </nav>
-        </header>
-    )
-  }
-  
-  export default Header;
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <NavLink to="/" className={styles.logoLink}>
+          <img
+            src="./images/memoji-7.PNG"
+            alt="profile"
+            className={styles.headerIcon}
+          />
+          <h3 className={styles.headerName}>
+            {props.title}
+          </h3>
+        </NavLink>
+      </div>
+
+      <nav>
+        <NavLink to="/" className={getNavLinkClass}>
+          Home
+        </NavLink>
+        <NavLink to="/about" className={getNavLinkClass}>
+          About Me
+        </NavLink>
+        <NavLink to="/projects" className={getNavLinkClass}>
+          Projects
+        </NavLink>
+        <NavLink to="/contact" className={getNavLinkClass}>
+          Contact
+        </NavLink>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
